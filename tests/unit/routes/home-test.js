46aa5b82bb46b2d052e802e769ec1561488b035f
Chris Coppola <mynamereallysux@gmail.com>
2017-04-12 13:14:49 -0400

    Initialized tests, no content yet.

---
+++ tests/unit/routes/home-test.js
@@ -0,0 +1,19 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:home', 'Unit | Route | home', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
+
+test('should show 8 planets', function(assert) {
+
+});
+
+test('planets should be sortable', function(assert) {
+
+});
